110f46131c82e12f99d07891f9ebbbc4c94211ce
stamo15 <stamo15@alustudent.com>
2018-06-20 17:36:35 +0400

    Add files via upload

---
+++ Part-1/Problem-Set-0/ps0Solution.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul  6 01:53:50 2017
+
+@author: Student
+"""
+import numpy
+x = int (input('Enter number x: '))
+y = int (input('Enter number y: '))
+print('x**y = ', x**y)
+print('log(x) = ', numpy.log2(x))
